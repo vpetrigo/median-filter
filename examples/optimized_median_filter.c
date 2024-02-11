eedc05bfa2b29302aaf14aea192e55fe7729625f
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2024-02-11 01:32:12 +0100

    optimized_median_filter: Solution that work with limitation on the minimum inserted value

--- examples/optimized_median_filter.c
+++ examples/optimized_median_filter.c
@@ -13,6 +13,7 @@ void optimized_mf_init(struct optimized_median_filter *filter, struct median_fil
 {
     filter->head.next = &filter->tail;
     filter->tail.next = NULL;
+    filter->tail.value = 0;
     filter->median = NULL;
     filter->buffer = buffer;
     filter->size = buffer_size;
@@ -22,50 +23,73 @@ void optimized_mf_init(struct optimized_median_filter *filter, struct median_fil
 
 void optimized_mf_insert_value(struct optimized_median_filter *filter, unsigned value)
 {
-    struct median_filter_node *scan_old = &filter->head;
-    struct median_filter_node *scan = filter->head.next;
+    struct median_filter_node *scan_old = NULL;
+    /* Points to pointer to first (largest) datum in chain */
+    struct median_filter_node *scan = &filter->head;
     struct median_filter_node *insert = &filter->buffer[filter->current_pos];
     struct median_filter_node *insert_next = insert->next;
     const struct median_filter_node *const end = &filter->tail;
 
-    insert->value = value;
     filter->current_pos = (filter->current_pos + 1) % filter->size;
+    insert->value = value;
+    /* Median initially to first in chain */
+    filter->median = &filter->head;
 
-    if (scan == &filter->tail) {
-        insert->next = scan;
-        scan_old->next = insert;
-    }
-
+    /* Handle chain-out of first item in chain as special case */
     if (scan->next == insert) {
         scan->next = insert_next;
     }
 
-    while (scan != end) {
-        if (scan->value < insert->value) {
-            scan_old->next = insert;
-            insert->next = scan;
+    scan_old = scan;
+    scan = scan->next;
+
+    /* Loop through the chain, normal loop exit via break. */
+    for (size_t i = 0; i < filter->size; ++i) {
+        /* Handle odd-numbered item in chain  */
+        if (scan->next == insert) {
+            scan->next = insert_next; /* Chain out the old datum.*/
+        }
+
+        if (scan->value < value) /* If datum is larger than scanned value,*/
+        {
+            insert->next = scan_old->next; /* Chain it in here.  */
+            scan_old->next = insert;       /* Mark it chained in. */
+            value = 0;
+        };
+
+        /* Step median pointer down chain after doing odd-numbered element */
+        filter->median = filter->median->next; /* Step median pointer.  */
+
+        if (scan == end) {
             break;
         }
 
-        scan_old = scan;
-        scan = scan->next;
+        scan_old = scan;   /* Save this pointer and   */
+        scan = scan->next; /* step down chain */
+
+        /* Handle even-numbered item in chain.  */
+        if (scan->next == insert) {
+            scan->next = insert_next;
+        }
+
+        if (scan->value < value) {
+            insert->next = scan_old->next; /* Chain it in here.  */
+            scan_old->next = insert;       /* Mark it chained in. */
+            value = 0;
+        }
+
+        if (scan == end) {
+            break;
+        }
+
+        scan_old = scan;   /* Save this pointer and   */
+        scan = scan->next; /* step down chain */
     }
 }
 
 unsigned optimized_mf_get_median(const struct optimized_median_filter *filter)
 {
-    const struct median_filter_node *current = filter->head.next;
-    const struct median_filter_node *const end = &filter->tail;
-
-    if (current == end) {
-        return 0;
-    }
-
-    for (size_t i = 0; i < filter->size / 2 && current->next != end; ++i) {
-        current = current->next;
-    }
-
-    return current->value;
+    return filter->median->value;
 }
 
 void optimized_mf_debug(const struct optimized_median_filter *filter)
@@ -78,7 +102,7 @@ void optimized_mf_debug(const struct optimized_median_filter *filter)
         printf("%u ", filter->buffer[i].value);
     }
 
-    puts("Print ordered list:");
+    puts("\nPrint ordered list:");
     while (current != end) {
         printf("%u", current->value);
         current = current->next;
